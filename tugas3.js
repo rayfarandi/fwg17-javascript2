067655affc3ac06e010f733b9a4ba25f645cd8ed
Ran Ran <43269555+rayfarandi@users.noreply.github.com>
2023-10-27 04:58:12 +0700

    tugas

--- tugas3.js
+++ tugas3.js
@@ -1,12 +1,26 @@
-function seleksiNilai(nilaiAwal, nilaiAkhir, nilai) {
-    const hasilNilai = nilai.filter(function(hasil) {
-        return hasil >= nilaiAwal && hasil <= nilaiAkhir;
-    });
-    return hasilNilai;
+function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {   
+    const hasilSeleksi = dataArray.filter((nilai) => nilai >= nilaiAwal && nilai <= nilaiAkhir);
+    hasilSeleksi.sort((a,b)=>a-b)
+    if (nilaiAkhir<=nilaiAwal){
+        return "Nilai Akhir Harus Lebih Besar Dari Nilai Awal"
+    }
+
+    if (dataArray.length <= 5){
+        return "Jumlah Angka Dalam Array Harus Lebih Dari 5"
+    }
+
+    
+    if (hasilSeleksi.length===0){
+        return "nilai tidak di temukan"
+    }
+
+    return hasilSeleksi;
+    
 }
 
-const nilai = [2, 25, 4, 14, 17, 30, 8];
-const nilaiAwal = 5;
-const nilaiAkhir = 20;
-const tersaring = seleksiNilai(nilaiAwal, nilaiAkhir, nilai);
-console.log(tersaring);
+
+
+console.log(seleksiNilai(5,20,[2,25,14,17,30,8]));
+console.log(seleksiNilai(15,3,[2,25,4,14,17,30,8]));
+console.log(seleksiNilai(4,17,[2,25,4]));
+console.log(seleksiNilai(5,17,[2,25,4,1,30,18]));
